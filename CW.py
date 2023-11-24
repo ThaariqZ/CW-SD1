415b3f2fe7831327d3d07102b09f43621cae66ca
ThaariqZ <145918518+ThaariqZ@users.noreply.github.com>
2023-11-24 07:57:22 +0530

    Update CW.py

--- CW.py
+++ CW.py
@@ -1,3 +1,7 @@
+#Calling graphics.py
+from graphics import *
+
+#initializing variables
 option=""
 progress=0
 module_trailer=0
@@ -7,6 +11,64 @@ pass_mark=0
 defer_mark=0
 fail_mark=0
 total_marks=0
+
+def draw_histogram():
+    height_1=0
+    height_2=0
+    height_3=0
+    height_4=0
+
+    # Create a graphics window.
+    win = GraphWin("Histogram", 400, 300)
+
+    # Window
+    label = Text(Point(80, 24), 'Histogram Results')
+    label.draw(win)
+
+    #creating the 1st column
+    height_1=280-(progress*10)
+    label = Text(Point(60, (height_1-15)), progress)
+    box = Rectangle(Point(24, 280), Point(96, height_1)) # Height is 14 * 10 = (280 - 140) = 140
+    box.setFill("blue")
+    box.draw(win)
+    label.draw(win)
+
+    #creating 2nd column
+    height_2=280-(module_trailer*10)
+    label = Text(Point(145, (height_2-15)), module_trailer)
+    box = Rectangle(Point(106, 280), Point(178, height_2)) # Height is 10 * 10 = 100
+    box.setFill("red")
+    box.draw(win)
+    label.draw(win)
+
+    #creating 3rd column
+    height_3=280-(module_retriever*10)
+    label = Text(Point(230, (height_3-15)), module_retriever)
+    box = Rectangle(Point(188, 280), Point(260, height_3)) # Height is 7 * 10 = 70
+    box.setFill("green")
+    box.draw(win)
+    label.draw(win)
+
+    #creating 4th column
+    height_4=280-(exclude*10)
+    label = Text(Point(310, (height_4-15)), exclude)
+    box = Rectangle(Point(270, 280), Point(342, height_4)) # Height is 2 * 10 = 20
+    box.setFill("pink")
+    box.draw(win)
+    label.draw(win)
+
+    #naming the  columns
+    label = Text(Point(60, 290), "Progress")
+    label.draw(win)
+    label = Text(Point(145, 290), "Trailer")
+    label.draw(win)
+    label = Text(Point(230, 290), "Retriever")
+    label.draw(win)
+    label = Text(Point(310, 290), "exclude")
+    label.draw(win)
+
+    win.getMouse()
+    win.close()
 
 def get_valid_mark(marks):
     while True:
@@ -54,4 +116,7 @@ while True:
         break
     else:
         print("Invalid Input \n")
-        # Now you can use pass_mark, defer_mark, and fail_mark in your coded
+
+
+draw_histogram()
+
